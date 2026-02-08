95153c8cf838eb44a5e616b14f9cbdd5dbb040bc
Motomasa511 <murata-mocchee0511@g.ecc.u-tokyo.ac.jp>
2026-02-08 22:35:09 +0900

    Update: 2026-02-08 22:35:09

--- 4_etas/test.f
+++
@@ -1,9 +0,0 @@
-      subroutine add_numbers(a, b, result)
-Cf2py intent(in) a, b
-Cf2py intent(out) result
-      implicit none
-      double precision a, b, result
-      result = a + b
-      return
-      end
-    
